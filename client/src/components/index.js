b23af9cded1205c83ab209e8d358d79aaf963fa4
Toba-dean <tobaogundimu@gmail.com>
2023-08-27 12:34:11 +0100

    done with the home page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,1 +1,7 @@
 export { default as NavBar } from "./navbar/Navbar";
+export { default as Header } from "./header/Header";
+export { default as Featured } from "./featured/Featured";
+export { default as PropertyList } from "./propertylist/PropertyList";
+export { default as FeaturedProperties } from "./featuredproperties/FeaturedProperties";
+export { default as MailList } from "./mailList/MailList";
+export { default as Footer } from "./footer/Footer";
--- client/src/pages/home/index.jsx
+++ client/src/pages/home/index.jsx
@@ -1,12 +1,28 @@
-import React from "react";
-
 import "./home.styles.css";
-import { NavBar } from "../../components";
+import {
+  Featured,
+  FeaturedProperties,
+  Footer,
+  Header,
+  MailList,
+  NavBar,
+  PropertyList,
+} from "../../components";
 
 const Home = () => {
   return (
     <>
       <NavBar />
+      <Header />
+      <div className="homeContainer">
+        <Featured />
+        <h1 className="homeTitle">Browse by property type</h1>
+        <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
+        <MailList />
+        <Footer />
+      </div>
     </>
   );
 };
