68b9c6998ae2a7185a62ffeeda315b35c8acdce4
Toba-dean <tobaogundimu@gmail.com>
2023-08-27 21:02:17 +0100

    done with the lsitings page

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -5,3 +5,4 @@ export { default as PropertyList } from "./propertylist/PropertyList";
 export { default as FeaturedProperties } from "./featuredproperties/FeaturedProperties";
 export { default as MailList } from "./mailList/MailList";
 export { default as Footer } from "./footer/Footer";
+export { default as SearchItem } from "./searchitem/SearchItem";
